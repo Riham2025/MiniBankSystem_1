8da68d593d9f7edc5b57235702f1228fedcbb5bd
riham <riham91.alsiyabi@gmail.com>
2025-07-02 16:01:10 +0400

    Update the User-operations menu text and Extend the switch in UserOperations()

--- Program.cs
+++ Program.cs
@@ -134,6 +134,7 @@ namespace MiniBankSystem_1
                     case "3": CheckBalance(); Console.ReadLine(); break;
                     case "4": submitReview(); Console.ReadLine(); break;
                     case "5": TransferFunds(); Console.ReadLine(); break;
+                    case "6": UndoLastComplaint(); Console.ReadLine(); break;
                     case "0":
                         inUserMenu = false;
                         Logout();               // end session
