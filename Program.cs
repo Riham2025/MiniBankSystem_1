12137cc32100baf2b7293a12eafc6fe75143ed10
riham <riham91.alsiyabi@gmail.com>
2025-07-06 11:38:28 +0400

    add to the admin menu >> case "11": ShowAverageFeedback(); Console.ReadLine(); break;

--- Program.cs
+++ Program.cs
@@ -226,6 +226,8 @@ namespace MiniBankSystem_1
                     case "8": ShowTotalBankBalance(); Console.ReadLine(); break;
                     case "9": ExportAccountsToCsv(); Console.ReadLine(); break;
                     case "10":ApproveRejectLoans(); Console.ReadLine(); break;
+                    case "11":ShowAverageFeedback(); Console.ReadLine(); break;
+
 
 
                         Console.ReadLine();
